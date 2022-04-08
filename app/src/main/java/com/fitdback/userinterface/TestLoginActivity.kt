c198361009d06a0a7a84b902a6aeb2775d2a09b4
Balparang <shineecard5@gmail.com>
2022-04-08 13:43:26 +0900

    에러: TestLoginActivity - Firebase.auth 에서 Unresolved reference: auth

--- app/src/main/java/com/fitdback/userinterface/TestLoginActivity.kt
+++ app/src/main/java/com/fitdback/userinterface/TestLoginActivity.kt
@@ -9,13 +9,21 @@ import android.widget.EditText
 import android.widget.Toast
 import com.fitdback.posedetection.CameraActivity
 import com.fitdback.posedetection.R
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.ktx.Firebase
 
 class TestLoginActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_test_login)
+
+        // 해당 라인 'Firebase.' 뒤의 'auth'가 자동 import 되지 않고 빨간줄로 표시 됨.
+        // 팝업 메시지 : Unresolved reference: auth
+        auth = Firebase.auth
 
         val btnLogin = findViewById<Button>(R.id.btnLogin)
         val btnjoin = findViewById<Button>(R.id.btnJoin)
@@ -35,9 +43,26 @@ class TestLoginActivity : AppCompatActivity() {
 
         }
 
-        btnAnonymousLogin.setOnClickListener{
+        btnAnonymousLogin.setOnClickListener {
 
             Toast.makeText(this, "Anonymous Login", Toast.LENGTH_LONG).show()
+
+            auth.signInAnonymously()
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        val user = auth.currentUser
+
+                        Log.d("TestLogin", user!!.uid)
+
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Toast.makeText(
+                            baseContext, "Authentication failed.",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
 
         }
 
