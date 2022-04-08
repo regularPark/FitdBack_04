8bbbe7c72f97dc509128b8ab0dfccfa0767b7443
Balparang <shineecard5@gmail.com>
2022-04-08 12:02:11 +0900

    [추가] Login액티비티 기능 추가

--- app/src/main/java/com/fitdback/userinterface/TestLoginActivity.kt
+++ app/src/main/java/com/fitdback/userinterface/TestLoginActivity.kt
@@ -1,7 +1,12 @@
 package com.fitdback.userinterface
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import com.fitdback.posedetection.CameraActivity
 import com.fitdback.posedetection.R
 
 class TestLoginActivity : AppCompatActivity() {
@@ -10,6 +15,23 @@ class TestLoginActivity : AppCompatActivity() {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_test_login)
+
+        val btnLogin = findViewById<Button>(R.id.btnLogin)
+        var btnjoin = findViewById<Button>(R.id.btnJoin)
+
+        var toCameraIntent = Intent(this, CameraActivity::class.java)
+
+        btnLogin.setOnClickListener {
+
+            val email = findViewById<EditText>(R.id.areaID)
+            val password = findViewById<EditText>(R.id.areaPassword)
+
+            Log.d("tla", email.text.toString())
+            Log.d("tla", password.text.toString())
+
+            startActivity(toCameraIntent)
+
+        }
 
     }
 }
