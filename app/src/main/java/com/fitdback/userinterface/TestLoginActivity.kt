dc4041a5269feadf757bbbe927bc2a12ad37c627
Balparang <shineecard5@gmail.com>
2022-04-08 13:05:48 +0900

    androidx 마이그레이션

--- app/src/main/java/com/fitdback/userinterface/TestLoginActivity.kt
+++ app/src/main/java/com/fitdback/userinterface/TestLoginActivity.kt
@@ -1,7 +1,7 @@
 package com.fitdback.userinterface
 
 import android.content.Intent
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
