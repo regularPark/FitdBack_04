a17c2d967ff3e68b8d1678e77b2c47c0737637dc
JayPark0110 <90092440+JayPark0110@users.noreply.github.com>
2022-05-05 15:45:37 +0900

    MainActivity의 Fragment화
    기존 MainActivity를 BottomNavigationBar를 이용하여 Home, MyPage, MyTown 프래그먼트 형식으로 변경

---
+++ app/src/main/java/com/fitdback/userinterface/MainTestActivity.kt
@@ -0,0 +1,64 @@
+package com.fitdback.userinterface
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.FrameLayout
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.viewpager2.widget.ViewPager2
+import com.fitdback.posedetection.R
+import com.fitdback.userinterface.fragment.HomeFragment
+import com.fitdback.userinterface.fragment.MyPageFragment
+import com.fitdback.userinterface.fragment.MyTownFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainTestActivity : AppCompatActivity() {
+
+    private val vp: ViewPager2 by lazy { findViewById(R.id.vp_) }   // TODO:빨간줄 무시할 것!!! 고치면 실행 안됨
+
+    private val bn: BottomNavigationView by lazy { findViewById(R.id.bn_) }
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_test)
+
+        vp.apply {
+            adapter = ViewPagerAdapter(this@MainTestActivity)
+            registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+                override fun onPageSelected(position: Int) {
+                    super.onPageSelected(position)
+                    bn.selectedItemId = when (position) {
+                        0 ->R.id.menu_home
+                        1 ->R.id.menu_mytown
+                        else -> R.id.menu_mypage
+                    }
+                }
+            })
+        }
+
+        bn.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.menu_home -> vp.currentItem = 0
+                R.id.menu_mytown -> vp.currentItem = 1
+                else -> vp.currentItem = 2
+            }
+            true
+        }
+    }
+
+    inner class ViewPagerAdapter(fa: FragmentActivity) : FragmentStateAdapter(fa) {
+        override fun getItemCount() = 3
+
+        override fun createFragment(position: Int): Fragment {
+            return when (position) {
+                0 -> HomeFragment()
+                1 -> MyTownFragment()
+                else -> MyPageFragment()
+            }
+        }
+    }
+
+
+}
---
+++ app/src/main/java/com/fitdback/userinterface/fragment/MyPageFragment.kt
@@ -0,0 +1,101 @@
+package com.fitdback.userinterface.fragment
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import com.fitdback.posedetection.R
+import com.fitdback.userinterface.StatisticsPopUpActivity
+import com.fitdback.userinterface.UserInformationActivity
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [MyPageFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class MyPageFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+    private val loginInfoBtn: Button? = null
+    private val statisticBtn: Button? = null
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+
+        }
+
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        val view = inflater.inflate(R.layout.fragment_my_page, container, false)
+
+
+        val statisticBtn : Button = view.findViewById(R.id.statBtn)
+        // 마이 페이지 프래그먼트 -> 액티비티 전환
+        val loginInfoBtn : Button = view.findViewById(R.id.loginInfoBtn)
+        loginInfoBtn.setOnClickListener(object :View.OnClickListener {
+            override fun onClick(v: View?) {
+                val intent = Intent(context, UserInformationActivity::class.java)
+                startActivity(intent)
+            }
+        })
+
+        statisticBtn.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(p0: View?) {
+                val toStaticsPopupActivityIntent = Intent(context, StatisticsPopUpActivity::class.java)
+                startActivity(toStaticsPopupActivityIntent)
+            }
+        })
+
+
+        // Inflate the layout for this fragment
+        return view
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment MyPageFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            MyPageFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+
+
+}
